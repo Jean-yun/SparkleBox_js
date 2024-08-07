f8e1c1d62f0e6229e9e4819b32701e03739ec7ea
Jean-yun <yesirchloe@gmail.com>
2024-08-07 20:22:07 +0900

    added functions to record input chagnes and submit button

--- src/App.js
+++ src/App.js
@@ -5,7 +5,36 @@ function App() {
   //Add Single compliment and List of compliments
   const [word, setWord] = useState("");
   const [words, setWords] = useState([]);
+  const [selectedSector, setSelectedSector] = useState("Family & Friends"); //default setting
 
+  //Handle sector change
+  const handleSectorChange = (event) => {
+    setSelectedSector(event.target.value);
+  };
+  
+  //When changed input, the word changes
+  const handleInputChange = (event) => {
+    setWord(event.target.value)
+  }
+  
+  
+  //When submitted, add the compliment to the selected sector
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    //if the word is valid after removin spaces on each end 
+    if (word.trim()) {
+      //add the compliment to the end of the selected Sector list
+      setWords((prevWords) => [...prevWords, { text: word, sector: selectedSector }]);
+      //reset the input field
+      setWord("")
+    }
+  };
+  
+  const submitCompliment = (event) => {
+    setWord(event.target.value)
+    console.log(event.target.value)
+  };
+    
   return (
     <div className="App">
       <h1>Hi</h1>
@@ -21,7 +50,8 @@ function App() {
         <button>Career</button>
       </div>
       
-      <select>
+      <form onSubmit={handleSubmit}>
+      <select onChange={handleSectorChange} value={selectedSector}>
         <option>Family & Friends</option>
         <option>Lover</option>
         <option>Personal Growth</option>
@@ -30,10 +60,20 @@ function App() {
         <option>Health</option>
         <option>Finance</option>
         <option>Career</option>
-      </select>
-      <input type="text"></input>
-      <button>Submit</button>
+        </select>
+
+        <input
+          type="text"
+          placeholder="Write your today's compliment :) "
+          value={word}
+          onChange={handleInputChange}
+        />
+                
+        <button type="submit">Submit</button>
+      </form>
     </div>
+    
+      
   );
 }
 
